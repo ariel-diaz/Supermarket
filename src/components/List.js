726348660b328c4e00b8f2c11168210468a55d92
Ariel Diaz <ariel.diaz@aerolab.co>
2019-11-15 01:11:21 -0300

    Add responsive

--- src/components/List.js
+++ src/components/List.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 function List({ list, deleteItem }) {
   return (
-    <div className="Container-list">
-      {list.length === 0 ? (
-        <div className="Container-list-message">
-          Oops! You dont have items in the list, But you can add one!
-        </div>
-      ) : (
-        list.map(({ id, title }) => <Item key={id} id={id} title={title} deleteItem={deleteItem} />)
-      )}
+    <div className="Container-scrollbox">
+      <div className="Container-list">
+        {list.length === 0 ? (
+          <div className="Container-list-message">
+            Oops! You dont have items in the list, But you can add one!
+          </div>
+        ) : (
+          list.map(({ id, title }) => (
+            <Item key={id} id={id} title={title} deleteItem={deleteItem} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
